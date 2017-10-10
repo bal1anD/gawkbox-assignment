0aff8dada8832589de3c47b6abc4cb30829ed4f0
Dibyendu Mukherjee <dibyendumukherjee@Dibyendus-MacBook-Pro.local>
2017-10-09 18:26:32 -0700

    adding the DefaultPathController in the router

--- myrouter/router.go
+++ myrouter/router.go
@@ -21,6 +21,8 @@ func Route(w http.ResponseWriter, r *http.Request) {
 		controllers.GetChannelInfoController(w,p[2])
 	}else if (p[1]=="users" && p[3]=="info") {
 		controllers.GetUserInfoController(w,p[2])
+	} else {
+		controllers.DefaultPathController(w)
 	}
 }
 
